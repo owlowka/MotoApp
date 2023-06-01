7e35178754d8a7c6cb3bfdd4966cf50a38e2e022
owlowka <roksana.maria.duda@gmail.com>
2023-06-01 16:01:04 +0200

    Creating T type BasicStack with Pop and Push methods.

---
+++ MotoApp/MotoApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using MotoApp;
+
+var stack = new Stack<double>();
+
+var stackString = new Stack<string>();
+
+var stackInt = new Stack<int>();
+
+stack.Push(item: 4.8);
+stack.Push(item: 43);
+stack.Push(item: 333.4);
+
+stackString.Push(item: "company");
+
+stackInt.Push(item: 12);
+
+double sum = 0.0;
+
+while (stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
+
+Console.WriteLine($"Sum: {sum}");
